3a1852d2829b464761293a62aa8f7013e919c348
aridder <aridder@pm.me>
2023-11-11 21:46:34 +0300

    "proving" jwt age with guest and host

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,25 +1,74 @@
 #![no_main]
+
+use ed25519_dalek::VerifyingKey;
+use jwt_compact::{alg::*, prelude::*, Token, UntrustedToken};
 // If you want to try std support, also update the guest Cargo.toml file
-use k256::{
-    ecdsa::{signature::Verifier, Signature, VerifyingKey},
-    EncodedPoint,
-};
 use risc0_zkvm::guest::env;
+use serde::{Deserialize, Serialize};
 
 risc0_zkvm::guest::entry!(main);
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct CredentialSubject {
+    #[serde(rename = "dateOfBirth")]
+    date_of_birth: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Issuer {
+    id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Proof {
+    #[serde(rename = "type")]
+    proof_type: String,
+    jwt: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct VerifiableCredential {
+    #[serde(rename = "credentialSubject")]
+    credential_subject: CredentialSubject,
+    issuer: Issuer,
+    #[serde(rename = "type")]
+    types: Vec<String>,
+    #[serde(rename = "@context")]
+    context: Vec<String>,
+    #[serde(rename = "issuanceDate")]
+    issuance_date: String,
+    proof: Proof,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct VC {
+    #[serde(rename = "credentialSubject")]
+    credential_subject: CredentialSubject,
+    #[serde(rename = "type")]
+    types: Vec<String>,
+    #[serde(rename = "@context")]
+    context: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct VCClaims {
+    vc: VC,
+    sub: String,
+    iss: String,
+}
+
+
 pub fn main() {
     // Decode the verifying key, message, and signature from the inputs.
-    let (encoded_verifying_key, message, signature): (EncodedPoint, Vec<u8>, Signature) =
-        env::read();
-        
-    let verifying_key = VerifyingKey::from_encoded_point(&encoded_verifying_key).unwrap();
+    let (age, jwt, public_key): (u32, String, String) = env::read();
+    let public_key_bytes: &[u8; 32] = &hex::decode(&public_key).unwrap().try_into().unwrap();
 
     // Verify the signature, panicking if verification fails.
-    verifying_key
-        .verify(&message, &signature)
-        .expect("ECDSA signature verification failed");
+    let verifying_key = VerifyingKey::from_bytes(&public_key_bytes).unwrap();
+    let token = UntrustedToken::new(&jwt).unwrap();
+    let token: Token<VCClaims> = Ed25519.validator(&verifying_key).validate(&token).unwrap();
+    println!("token: {:?}", token);
 
     // Commit to the journal the verifying key and message that was signed.
-    env::commit(&(encoded_verifying_key, message));
+    env::commit(&(age));
 }
